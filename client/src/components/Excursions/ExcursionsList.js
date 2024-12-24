1d43cfd54b8a2936e31cb593d36639c646a4091c
Arthailor <denart020@gmail.com>
2024-12-24 20:37:01 +0300

    change excursions menu

--- client/src/components/Excursions/ExcursionsList.js
+++ client/src/components/Excursions/ExcursionsList.js
@@ -3,17 +3,18 @@ import { Button, ListGroup } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
 import { deleteExcursion } from '../../http/modelAPI';
 
-
 export default function ExcursionsList({ excursions, classes, trips }) {
     const { employee } = useSelector((state) => {
         return state.employees;
-    })
+    });
+
     const delExcursion = (excursionId) => {
-        deleteExcursion(excursionId).finally(() => window.location.reload())
-    }
+        deleteExcursion(excursionId).finally(() => window.location.reload());
+    };
+
     return (
         <div>
-            {excursions.length === 0 ?
+            {excursions.length === 0 ? (
                 <ListGroup>
                     <ListGroup.Item
                         className="d-flex justify-content-between"
@@ -21,29 +22,38 @@ export default function ExcursionsList({ excursions, classes, trips }) {
                         Empty list
                     </ListGroup.Item>
                 </ListGroup>
-                :
+            ) : (
                 <ListGroup>
-                    {excursions.map(exc =>
-                        <ListGroup.Item
-                            key={exc.excursion_id}
-                            className="d-flex justify-content-between"
-                        >
-                            {classes.length !== 0 && trips.length !== 0 ?
-                                <div style={{ display: 'flex', alignItems: "center" }}>
-                                    {classes.find(cls => cls.class_id === exc.classClassId).name} - {trips.find(trp => trp.trip_id === exc.tripTripId).point_of_interest} ({exc.date})
-                                </div>
-                                :
-                                ''
-                            }
-                            {employee.employee_id === "ADMIN" ?
-                                <Button className="m-1 " variant="outline-danger" onClick={() => { delExcursion(exc.excursion_id) }}>Delete</Button>
-                                :
-                                ''
-                            }
-                        </ListGroup.Item>
-                    )}
+                    {excursions.map(exc => {
+                        const classInfo = classes.find(cls => cls.class_id === exc.classClassId);
+                        const tripInfo = trips.find(trp => trp.trip_id === exc.tripTripId);
+
+                        return (
+                            <ListGroup.Item
+                                key={exc.excursion_id}
+                                className="d-flex justify-content-between"
+                            >
+                                {classInfo && tripInfo ? (
+                                    <div style={{ display: 'flex', alignItems: "center" }}>
+                                        {classInfo.name} - {tripInfo.point_of_interest} ({exc.date})
+                                    </div>
+                                ) : (
+                                    <div>Data not available</div>
+                                )}
+                                {employee.employee_id === "ADMIN" && (
+                                    <Button
+                                        className="m-1"
+                                        variant="outline-danger"
+                                        onClick={() => { delExcursion(exc.excursion_id); }}
+                                    >
+                                        Delete
+                                    </Button>
+                                )}
+                            </ListGroup.Item>
+                        );
+                    })}
                 </ListGroup>
-            }
+            )}
         </div>
-    )
+    );
 }
